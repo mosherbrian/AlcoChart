7a51e47cbbdc1394fd7fe737bacd9158c4ac1b6c
mosherbrian <mosher.brian@gmail.com>
2023-08-17 11:40:01 -0700

    Revert "aider: Fixed issue with `setPreset` function not finding elements with IDs `drinkSize` and `alcoholStrength` by wrapping the function in a `window.onload` event handler."
    This reverts commit bb7543f123becb371afb514c312c6a7ccea26cbd.

--- app.js
+++ app.js
@@ -4,34 +4,32 @@ var dailyAlcoholIntake = 0;
 var drinkSize = 5;
 var alcoholStrength = 5;
 
-window.onload = function() {
-    function setPreset(preset) {
-        switch(preset) {
-            case 'beerGlass':
-                drinkSize = 12;
-                alcoholStrength = 5;
-                break;
-            case 'beerPint':
-                drinkSize = 16;
-                alcoholStrength = 5;
-                break;
-            case 'bigBeer':
-                drinkSize = 20;
-                alcoholStrength = 5;
-                break;
-            case 'wineGlass':
-                drinkSize = 5;
-                alcoholStrength = 12;
-                break;
-            case 'cocktail':
-                drinkSize = 2;
-                alcoholStrength = 40;
-                break;
-        }
-        document.getElementById('drinkSize').value = drinkSize;
-        document.getElementById('alcoholStrength').value = alcoholStrength;
-        updateAlcoholCount();
+function setPreset(preset) {
+    switch(preset) {
+        case 'beerGlass':
+            drinkSize = 12;
+            alcoholStrength = 5;
+            break;
+        case 'beerPint':
+            drinkSize = 16;
+            alcoholStrength = 5;
+            break;
+        case 'bigBeer':
+            drinkSize = 20;
+            alcoholStrength = 5;
+            break;
+        case 'wineGlass':
+            drinkSize = 5;
+            alcoholStrength = 12;
+            break;
+        case 'cocktail':
+            drinkSize = 2;
+            alcoholStrength = 40;
+            break;
     }
+    document.getElementById('drinkSize').value = drinkSize;
+    document.getElementById('alcoholStrength').value = alcoholStrength;
+    updateAlcoholCount();
 }
 
 function updateDrinkSize(value) {
