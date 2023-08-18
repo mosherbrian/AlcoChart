49a13cab31514db17db3a084b73c03b5124fb45c
mosherbrian <mosher.brian@gmail.com>
2023-08-18 08:59:40 -0700

    aider: Added options to the Type dropdown and made the Size and Strength dropdowns update based on the Type selection.
    # Aider chat conversation:
    
    USER: OKay, please continue.
    
    ASSISTANT: Great! Now let's move on to the third task: adding options to the Type dropdown and making the Size and Strength dropdowns update based on the Type selection.
    
    To do this, we need to modify the `design2.js` file. We need to add an event listener to the Type dropdown that updates the options of the Size and Strength dropdowns when a new type is selected.
    
    Here's how we can do it:
    
    1. First, we need to define the options for each type. We can do this by creating an object where the keys are the types and the values are objects with the size and strength options.
    
    2. Next, we need to add an event listener to the Type dropdown that updates the Size and Strength dropdowns when a new type is selected. We can do this by creating a function that clears the options of a dropdown and then adds new options to it.
    
    3. Finally, we need to call this function when the page loads to populate the Type dropdown with the initial options.
    
    Here's the *edit block* for these changes:
    
    ```javascript
    design2.js
    <<<<<<< HEAD
    var typeDropdown = document.getElementById('typeDropdown');
    var sizeDropdown = document.getElementById('sizeDropdown');
    var strengthDropdown = document.getElementById('strengthDropdown');
    
    typeDropdown.addEventListener('change', updateScore);
    sizeDropdown.addEventListener('change', updateScore);
    strengthDropdown.addEventListener('change', updateScore);
    =======
    var typeOptions = {
        'Beer': { size: ['12oz', '16oz', '22oz'], strength: ['4%', '5%', '6%'] },
        'Wine': { size: ['5oz', '10oz'], strength: ['12%', '14%', '16%'] },
        'Cocktail': { size: ['2oz', '3oz', '4oz'], strength: ['20%', '30%', '40%'] }
    };
    
    var typeDropdown = document.getElementById('typeDropdown');
    var sizeDropdown = document.getElementById('sizeDropdown');
    var strengthDropdown = document.getElementById('strengthDropdown');
    
    typeDropdown.addEventListener('change', function() {
        updateDropdown(sizeDropdown, typeOptions[this.value].size);
        updateDropdown(strengthDropdown, typeOptions[this.value].strength);
        updateScore();
    });
    
    sizeDropdown.addEventListener('change', updateScore);
    strengthDropdown.addEventListener('change', updateScore);
    
    function updateDropdown(dropdown, options) {
        dropdown.innerHTML = '';
        options.forEach(function(option) {
            var optionElement = document.createElement('option');
            optionElement.textContent = option;
            dropdown.appendChild(optionElement);
        });
    }
    
    updateDropdown(typeDropdown, Object.keys(typeOptions));
    >>>>>>> updated
    ```
    
    Please apply these changes and let me know if you have any questions.

--- design2.js
+++ design2.js
@@ -1,13 +1,35 @@
 // Your JavaScript code will go here
 var dailyAlcoholIntake = 0;
 
+var typeOptions = {
+    'Beer': { size: ['12oz', '16oz', '22oz'], strength: ['4%', '5%', '6%'] },
+    'Wine': { size: ['5oz', '10oz'], strength: ['12%', '14%', '16%'] },
+    'Cocktail': { size: ['2oz', '3oz', '4oz'], strength: ['20%', '30%', '40%'] }
+};
+
 var typeDropdown = document.getElementById('typeDropdown');
 var sizeDropdown = document.getElementById('sizeDropdown');
 var strengthDropdown = document.getElementById('strengthDropdown');
 
-typeDropdown.addEventListener('change', updateScore);
+typeDropdown.addEventListener('change', function() {
+    updateDropdown(sizeDropdown, typeOptions[this.value].size);
+    updateDropdown(strengthDropdown, typeOptions[this.value].strength);
+    updateScore();
+});
+
 sizeDropdown.addEventListener('change', updateScore);
 strengthDropdown.addEventListener('change', updateScore);
+
+function updateDropdown(dropdown, options) {
+    dropdown.innerHTML = '';
+    options.forEach(function(option) {
+        var optionElement = document.createElement('option');
+        optionElement.textContent = option;
+        dropdown.appendChild(optionElement);
+    });
+}
+
+updateDropdown(typeDropdown, Object.keys(typeOptions));
 
 document.getElementById('quickSelections').addEventListener('click', function(event) {
     if (event.target.tagName === 'BUTTON') {
